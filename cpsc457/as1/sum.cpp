bf4f09353a54b206fa49d6a80776f4d1d1fcefa2
Andrew Helwer <andrew.helwer@gmail.com>
2012-10-12 11:03:40 -0600

    Added comments to sum.cpp

--- cpsc457/as1/sum.cpp
+++ cpsc457/as1/sum.cpp
@@ -1,7 +1,19 @@
+/**********************************************
+Last Name: Helwer
+First Name: Andrew
+Student ID: 10023875
+Course: CPSC 457
+Tutorial Section: T05
+Assignment: 1
+Question: 1
+File name: sum.cpp
+**********************************************/
+
 #include <iostream>
 #include <cstdio>
 #include <pthread.h>
 
+// Collection of fields to be associated with each thread
 struct SumRangeArgs {
     unsigned long low;
     unsigned long high;
@@ -9,6 +21,7 @@ struct SumRangeArgs {
     pthread_t id;
 };
 
+// Function called by each thread. Sums given range.
 void* SumRange(void* varg) {
     SumRangeArgs* arg = static_cast<SumRangeArgs*>(varg);
     for (unsigned long i = arg->low; i <= arg->high; ++i) {
@@ -17,16 +30,19 @@ void* SumRange(void* varg) {
 }
 
 int main(int argc, char* argv[]) {	
+    // Checks for correct program usage
     if (argc != 3) {
         std::cout << "Incorrect program usage." << std::endl;
         return 0;
     }
-    unsigned long N = strtoul(argv[1], NULL, 10);
-    unsigned long T = strtoul(argv[2], NULL, 10);
+    // Parses command line arguments
+    unsigned long N = strtoul(argv[1], NULL, 10); // Input size
+    unsigned long T = strtoul(argv[2], NULL, 10); // Thread count
     unsigned long blockSize = N/T;
 
+    // Spawns threads
     SumRangeArgs* args[T];
-    unsigned  long i = 0;
+    unsigned long i = 0;
     for (i = 0; i < T-1; ++i) {
         args[i] = new SumRangeArgs();
         args[i]->low = i*blockSize+1;
@@ -42,16 +58,24 @@ int main(int argc, char* argv[]) {
     int error = pthread_create(&(args[i]->id), NULL, SumRange, (void*)args[i]);
     printf("Thread %lu: (%lu, %lu)\n", args[i]->id, args[i]->low, args[i]->high);
 
+    // Waits for all threads to terminate
     for (unsigned long i = 0; i < T; ++i) {
         pthread_join(args[i]->id, NULL);
     }
 
+    // Sums up values returned by threads
     unsigned long sum = 0;
     for (unsigned long i = 0; i < T; ++i) {
         sum += args[i]->sum;
     }
+
+    // Prints out sum
     printf("Sum(1, %lu) = %lu\n", N, sum);
-    //printf("Sum(1, %lu) = %lu\n", N, (N*(N+1)/2));
+
+    // Frees allocated memory
+    for (unsigned long i = 0; i < T; ++i) {
+        delete args[i];
+    }
 
 	return 0;
 }
