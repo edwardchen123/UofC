871184d08cbc2d27737c0cda69f3451f281cdb16
ahelwer <andrew.helwer@gmail.com>
2012-11-05 00:17:23 -0700

    CPSC 535 Assignment 3 * filename now removed from top of image * Program now accepts input from command line

--- cpsc535/as3/as3.m
+++ cpsc535/as3/as3.m
@@ -1,11 +1,43 @@
-% Finds all connected features in image
-grayscale = imread("data00.tif");
+% Parses command line arguments
+%arg_list = argv();
+arg_list = {'data00.tif'};
+nargin = 1;
+imagefile = [];
+keys = [];
+trainingMode = 0;
+if (nargin == 2)
+	trainingMode = 1;
+	imagefile = arg_list{1};
+	keyfile = arg_list{2};
+	rawkeys = fileread(keyfile);
+	keys = reshape(rawkeys, 60, 25)';
+elseif (nargin == 1)
+	imagefile = arg_list{1};
+else
+	printf('ERROR: invalid number of arguments\n');
+endif
+
+printf('Finding image features... ');
+% Converts grayscale image to binary
+grayscale = imread(imagefile);
 q = quantile(grayscale(:), 0.20);
 threshold = q*ones(size(grayscale));
-binary = grayscale < threshold;
-[label, num] = bwlabel(binary, 8);
+binary = (grayscale < threshold);
 
-% Deals with each features separately
+% Removes filename from image
+hasFeatureOnRow = any(binary');
+edge = conv(hasFeatureOnRow, [-1 1]);
+newTop = find(edge == 1);
+binary = binary(newTop:rows(binary), :);
+grayscale = grayscale(newTop:rows(grayscale), :);
+
+% Gets features using provided bwlabel function
+[label, num] = bwlabel(binary, 8);
+printf('done. Found %d features.\n', num);
+
+printf('Mapping features to characters...\n');
+% Maps features to characters
+result = zeros(25, 60);
 %for i = 1:num,
 i = 16;
 	% Isolates each feature
@@ -24,11 +56,20 @@ i = 16;
 
 	% Calculates horizontal and vertical order
 	yMid = floor((yMin + yMax) / 2);
-	horizOrder = find(unique(label(yMid, :)) == i)-1;
+	xcol = find(unique(label(yMid, :)) == i)-1;
 	xMid = floor((xMin + xMax) / 2);
-	vertOrder = find(unique(label(:, xMid)) == i)-1;
+	xrow = find(unique(label(:, xMid)) == i)-1;
 
-%end
+	% Either trains OCR or OCRs input
+	if (trainingMode == 1)
+		%weights = TrainOCR(boundboxG, keys(xrow, xcol), weights);
+	else
+		%keys(xrow, xcol) = OCR(boundboxG, weights);
+	endif
+%endfor
+printf('done.\n');
 %mask = repmat(reshape(1:num, 1, 1, num), [rows(label) columns(label) 1]);
 %isolated = repmat(label, [1 1 num]) == mask;
 %clear mask;
+printf('Output in results.txt.\n');
+
---
+++ cpsc535/as3/ml.m
@@ -0,0 +1,10 @@
+weights = [0 0 0];
+
+function weights = TrainOCR(image, symbol, weights)
+
+endfunction
+
+function symbol = OCR(image, weights)
+
+endfunction
+
